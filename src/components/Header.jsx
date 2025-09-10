8e59996daa75cc5a5673b29fde6f085a98e6bc7a
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-09-10 10:49:09 +0200

    Crear hook personalizado y meter toda la logica de la web en ese hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
-import { useMemo } from "react"
+import { useCart } from "../hooks/useCart"
 
-function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart}) {
-    //State derivado
-    const isEmpty = useMemo( () => cart.length === 0, [cart] )
-    const cartTotal = useMemo( () => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
+function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal}) {
 
+    
     return (
         <header className="py-5 header">
             <div className="container-xl">
